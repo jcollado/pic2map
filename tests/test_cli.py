01a7bcdb33ade89db3a702c5be4ff9a64d204335
Javier Collado <jcollado@nowsecure.com>
2015-04-16 01:34:29 +0200

    Add parse arguments test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,13 @@ import os
 import tempfile
 import unittest
 
-from pic2map.cli import valid_directory
+from mock import patch
+
+from pic2map.cli import (
+    add,
+    parse_arguments,
+    valid_directory,
+)
 
 
 class ValidDirectoryTest(unittest.TestCase):
@@ -39,3 +45,17 @@ class ValidDirectoryTest(unittest.TestCase):
                 valid_directory(temp_directory)
         finally:
             os.rmdir(temp_directory)
+
+
+class ParseArgumentsTest(unittest.TestCase):
+
+    """Parse arguments test case."""
+
+    def test_add_command(self):
+        """Add command."""
+        directory = 'some directory'
+        with patch('pic2map.cli.valid_directory') as valid_directory_func:
+            valid_directory_func.return_value = directory
+            args = parse_arguments(['add', directory])
+            self.assertEqual(args.directory, directory)
+            self.assertEqual(args.func, add)
