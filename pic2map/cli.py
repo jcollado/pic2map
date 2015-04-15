85405d242b41b9dd721131c6e3046c47fe86847d
Javier Collado <jcollado@nowsecure.com>
2015-04-15 12:20:01 +0200

    Add command line option to set log level

--- pic2map/cli.py
+++ pic2map/cli.py
@@ -2,12 +2,34 @@
 """Command Line Interface."""
 
 import argparse
+import logging
 import sys
+
 
 def main(argv):
     """Entry point for the pic2map.py script."""
     args = parse_arguments(argv)
     print args
+
+
+def configure_logging(log_level):
+    """Configure logging based on command line argument.
+
+    :param log_level: Log level passed form the command line
+    :type log_level: int
+
+    """
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    # Log to sys.stderr using log level
+    # passed through command line
+    log_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)s: %(message)s')
+    log_handler.setFormatter(formatter)
+    log_handler.setLevel(log_level)
+    root_logger.addHandler(log_handler)
+
 
 def parse_arguments(argv):
     """Parse command line arguments.
@@ -18,7 +40,18 @@ def parse_arguments(argv):
     """
     parser = argparse.ArgumentParser(
         description='Display pictures location in a map')
+    log_levels = ['debug', 'info', 'warning', 'error', 'critical']
+    parser.add_argument(
+        '-l', '--log-level',
+        dest='log_level',
+        choices=log_levels,
+        default='warning',
+        help=('Log level. One of {0} or {1} '
+              '(%(default)s by default)'
+              .format(', '.join(log_levels[:-1]), log_levels[-1])))
+
     args = parser.parse_args(argv)
+    args.log_level = getattr(logging, args.log_level.upper())
     return args
 
 if __name__ == '__main__':
