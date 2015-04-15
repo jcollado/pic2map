ccfb70fb4c38ec0394ef6c7a82f87ab3707bc47a
Javier Collado <jcollado@nowsecure.com>
2015-04-15 12:28:09 +0200

    Add minimal implemenation of add subcommand

--- pic2map/cli.py
+++ pic2map/cli.py
@@ -3,13 +3,35 @@
 
 import argparse
 import logging
+import os
 import sys
+
+logger = logging.getLogger(__name__)
 
 
 def main(argv):
     """Entry point for the pic2map.py script."""
     args = parse_arguments(argv)
-    print args
+    configure_logging(args.log_level)
+    args.func(args)
+
+
+def add(args):
+    """Add images from a directory to the map."""
+    logger.info('Adding image files from %r...', args.directory)
+
+
+def valid_directory(path):
+    """Directory validation."""
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(
+            '{!r} is not a valid directory'.format(path))
+
+    if not os.access(path, os.R_OK | os.X_OK):
+        raise argparse.ArgumentTypeError(
+            'not enough permissions to explore {!r}'.format(path))
+
+    return path
 
 
 def configure_logging(log_level):
@@ -49,6 +71,14 @@ def parse_arguments(argv):
         help=('Log level. One of {0} or {1} '
               '(%(default)s by default)'
               .format(', '.join(log_levels[:-1]), log_levels[-1])))
+
+    subparsers = parser.add_subparsers(help='Subcommands')
+
+    add_parser = subparsers.add_parser(
+        'add', help='Add images')
+    add_parser.add_argument(
+        'directory', type=valid_directory, help='Base directory')
+    add_parser.set_defaults(func=add)
 
     args = parser.parse_args(argv)
     args.log_level = getattr(logging, args.log_level.upper())
