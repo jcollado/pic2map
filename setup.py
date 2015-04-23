4c3e1e74e56d60d284110094ae67d2affc57b375
Javier Collado <jcollado@nowsecure.com>
2015-04-23 12:06:34 +0200

    Set development status to alpha

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
