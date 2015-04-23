373b71a74b963358975e72447ccdb66aa6109651
Javier Collado <jcollado@nowsecure.com>
2015-04-23 11:23:16 +0200

    Add markers for every row in the map

--- pic2map/server/static/main.js
+++ pic2map/server/static/main.js
@@ -4,3 +4,9 @@ var map = L.map('map').setView([40.2001925,-89.0876265], 3);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+console.log('Adding markers for ' + rows.length + ' rows');
+rows.forEach(function(row) {
+    var coordinates = [row.latitude, row.longitude];
+    L.marker(coordinates).addTo(map);
+});
