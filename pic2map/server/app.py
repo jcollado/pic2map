b20ba1f436a6955b6f67871b0f9815ff1572f8d0
Javier Collado <jcollado@nowsecure.com>
2015-04-23 11:21:23 +0200

    Get location information rows and pass them to template
    Also, make sure that rows are json serializable (convert
    datetime.datetime objects to string)

--- pic2map/server/app.py
+++ pic2map/server/app.py
@@ -1,17 +1,46 @@
 # -*- coding: utf-8 -*-
 """Web application server."""
 
+import json
+
 from flask import (
     Flask,
     render_template,
 )
 
+from pic2map.db import LocationDB
+
+# Note: python and javascript don't seem to agree on what %c, %x
+# and %X are, so it's better to be explicit in the time formatting
+DATE_EXCHANGE_FORMAT = '%Y/%m/%d %H:%M:%S'
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """Application main page."""
-    return render_template('index.html')
+    with LocationDB() as location_db:
+        result = location_db.select_all()
+        rows = json.dumps([row_to_serializable(row) for row in result])
+
+    return render_template('index.html', rows=rows)
+
+
+def row_to_serializable(row):
+    """Transform row to make it json serializable.
+
+    This is needed to pass the location informaton to the javascript code.
+
+    :param row: Database row with location information
+    :type row: sqlalchemy.engine.result.RowProxy
+    :returns: Location information in JSON format
+    :rtype: dict(str)
+
+    """
+    row = dict(row)
+    row['datetime'] = row['datetime'].strftime(DATE_EXCHANGE_FORMAT)
+    return row
 
 
 if __name__ == '__main__':
