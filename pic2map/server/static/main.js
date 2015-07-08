64d6d484990829d79915e9a1fac7a0f24feb4dac
Javier Collado <javier.collado@gmail.com>
2015-07-08 07:17:25 +0200

    Use markercluster leaflet plugin
    This provides better visualization when there are a lot of markers in
    the map.

--- pic2map/server/static/main.js
+++ pic2map/server/static/main.js
@@ -1,17 +1,20 @@
 // Map centered in NowSecure HQ by default
 var map = L.map('map').setView([40.2001925,-89.0876265], 3);
+var markerCluster = L.markerClusterGroup();
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+map.addLayer(markerCluster);
 
 console.log('Adding markers for ' + rows.length + ' rows');
 rows.forEach(function(row) {
-    var coordinates = [row.latitude, row.longitude];
+    var marker = L.marker([row.latitude, row.longitude]);
     var text = 'Filename: ' + row.filename;
     if (row.datetime) {
       text += '<br>GPS datetime: ' + row.datetime;
     }
 
-    L.marker(coordinates).addTo(map).bindPopup(text);
+    marker.bindPopup(text);
+    markerCluster.addLayer(marker);
 });
