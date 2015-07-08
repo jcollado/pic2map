9af778058a7ac3dd6ef0f8b5497a8df27eadd76f
Javier Collado <javier.collado@gmail.com>
2015-07-08 08:03:13 +0200

    Update js code to be more object oriented

--- pic2map/server/static/js/main.js
+++ pic2map/server/static/js/main.js
@@ -1,20 +1,25 @@
-// Map centered in NowSecure HQ by default
-var map = L.map('map').setView([40.2001925,-89.0876265], 3);
-var markerCluster = L.markerClusterGroup();
+var LocationMap = {
+  'initialize': function initialize() {
+    // Map centered in NowSecure HQ by default
+    this.map = L.map('map').setView([40.2001925,-89.0876265], 3);
+    this.markerCluster = L.markerClusterGroup();
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-map.addLayer(markerCluster);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+    this.map.addLayer(this.markerCluster);
+  },
+  'addMarkers': function addMarkers(markersData) {
+    console.log('Adding ' + markersData.length + ' markers');
+    markersData.forEach(function(markerData) {
+        var marker = L.marker([markerData.latitude, markerData.longitude]);
+        var text = 'Filename: ' + markerData.filename;
+        if (markerData.datetime) {
+          text += '<br>GPS datetime: ' + markerData.datetime;
+        }
 
-console.log('Adding markers for ' + rows.length + ' rows');
-rows.forEach(function(row) {
-    var marker = L.marker([row.latitude, row.longitude]);
-    var text = 'Filename: ' + row.filename;
-    if (row.datetime) {
-      text += '<br>GPS datetime: ' + row.datetime;
-    }
-
-    marker.bindPopup(text);
-    markerCluster.addLayer(marker);
-});
+        marker.bindPopup(text);
+        this.markerCluster.addLayer(marker);
+    }, this);
+  }
+};
